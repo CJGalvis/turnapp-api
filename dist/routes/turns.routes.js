117d12e5683db112ef2db394d09da7b86d68a6f9
Camilo Galvis <cgalvis@facturecolombia.com>
2020-11-04 12:25:57 -0500

    fix route turn put

--- dist/routes/turns.routes.js
+++ dist/routes/turns.routes.js
@@ -5,7 +5,7 @@ const turns_controller_1 = require("../controllers/turns.controller");
 const router = express_1.Router();
 router.post('/turns/new', turns_controller_1.createTurn);
 router.get('/turns/get', turns_controller_1.getTurns);
-router.put('/turns/update/:_id', turns_controller_1.updateTurn);
+router.put('/turns/put/:_id', turns_controller_1.updateTurn);
 router.delete('/turns/delete/:_id', turns_controller_1.deleteTurns);
 exports.default = router;
 //# sourceMappingURL=turns.routes.js.map
